5e6340295f362fa7126bb311ccf219994aae5963
Vattghern1 <andre.2233@mail.ru>
2022-03-17 01:34:51 +0700

    Add create matrix and some other corrections

--- Task 1 A/A.js
+++ Task 1 A/A.js
@@ -9,7 +9,16 @@ function createMap(){
         for (var j = 0; j < mapSize; j++) {
             var td = document.createElement('td');
             td.classList.add("map-block");
+            if (i == 0 && j == 0) {
+                var lastBegin = td;
+                td.style.backgroundColor = "green";
+            }
+            if (i == 1 && j == 1) {
+                var lastEnd = td;
+                td.style.backgroundColor = "red";
+            }
             td.setAttribute("onclick", "chekerButtons(this)");
+            td.setAttribute("id", i+" - "+j)
             tr.appendChild(td);
         }
         table.appendChild(tr);
@@ -44,9 +53,11 @@ function chekerButtons(block) {
     switch (countSelectedButton) {
         case "Begin":
             setBegin(block);
+            lastBegin = block;
             break;
         case "End":
             setEnd(block);
+            lastEnd = block;
             break;
         case "Wall":
             setWall(block);
@@ -65,18 +76,51 @@ function setWall(block) {
     block.style.backgroundColor = "black";
 }
 
-function setEnd(block) {
-    block.style.backgroundColor = "red"
+function setEnd(block, lastEnd) {
+    lastEnd.style.backgroundColor = "white";
+    block.style.backgroundColor = "red";
 }
 
 function setBegin(block) {
-    block.style.backgroundColor = "green"
+    lastBegin.style.backgroundColor = "white";
+    block.style.backgroundColor = "green";
 }
 
 function createLabyrinth(mapSize) {
 
 }
 
-function findPath() {
+function findPath(mapSize) {
+    var matrix = createMatrix(mapSize);
 
+}
+
+function createMatrix(mapSize) {
+    mapSize = parseInt(mapSize);
+    var currentBlock;
+    var matrix = new Array(mapSize);
+    for (let i = 0; i < mapSize; i++) {
+        matrix[i] = new Array(mapSize);
+    }
+    for (let i = 0; i < mapSize; i++)
+    {
+        for (let j = 0; j < mapSize; j++) {
+            currentBlock = document.getElementById(i+" - "+j).style.backgroundColor;
+            switch (currentBlock) {
+                case ("black"):
+                    matrix[i][j] = -1;
+                    break;
+                case ("white"):
+                    matrix[i][j] = 0;
+                    break
+                case ("green"):
+                    matrix[i][j] = 1;
+                    break;
+                case ("red"):
+                    matrix[i][j] = 2;
+                    break;
+            }
+        }
+    }
+    return matrix;
 }
