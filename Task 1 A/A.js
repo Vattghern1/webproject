a7676270c656c03be123bea76ffa046107404ef3
Vattghern1 <andre.2233@mail.ru>
2022-03-16 21:43:07 +0700

    Adding visibility to begin and end

--- Task 1 A/A.js
+++ Task 1 A/A.js
@@ -5,9 +5,11 @@ function createMap(){
 
     for (var i = 0; i < mapSize; i++) {
         var tr = document.createElement('tr');
-
+        tr.classList.add("map-line");
         for (var j = 0; j < mapSize; j++) {
             var td = document.createElement('td');
+            td.classList.add("map-block");
+            td.setAttribute("onclick", "chekerButtons(this)");
             tr.appendChild(td);
         }
         table.appendChild(tr);
@@ -18,6 +20,51 @@ function clearMap() {
     document.querySelector('tbody').innerHTML = '';
 }
 
+function chooseWallBeginEnd(valueSelectedButton) {
+    var divButton = document.getElementById("selectButton");
+    switch (valueSelectedButton) {
+        case "Begin":
+            divButton.setAttribute("value", "Begin");
+            break;
+        case "End":
+            divButton.setAttribute("value", "End");
+            break;
+        default:
+            divButton.setAttribute("value", "Wall");
+    }
+}
+
+function chekerButtons(block) {
+    var divButtons = document.getElementById('selectButton');
+    var countSelectedButton = divButtons.getAttribute("value");
+    switch (countSelectedButton) {
+        case "Begin":
+            setBegin(block);
+            break;
+        case "End":
+            setEnd(block);
+            break;
+        default:
+            setWall(block);
+    }
+}
+
+function setWall(block) {
+    block.style.backgroundColor = "black";
+}
+
+function setEnd(block) {
+    block.style.backgroundColor = "red"
+}
+
+function setBegin(block) {
+    block.style.backgroundColor = "green"
+}
+
 function createLabyrinth(mapSize) {
+
+}
+
+function findPath() {
 
 }
