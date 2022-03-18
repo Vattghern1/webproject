924632b00ab14754894ffdbaecfd58c268e7a483
Dim-ulik <d.martyshev@yandex.ru>
2022-03-18 19:35:03 +0700

    Upd

--- Task 3 Clustering algorithm/Clustering algorithm.js
+++ Task 3 Clustering algorithm/Clustering algorithm.js
@@ -58,6 +58,7 @@ function generateColor() {
     return '#' + Math.floor(Math.random()*16777215).toString(16);
 }
 
+function 
 function clustering() {
     var counterClusters = document.getElementById("counterClusters").value;
     var centralPointsX = [];
