1af9053ef1c597c1976630c02742f4883bc903b1
Dim-ulik <d.martyshev@yandex.ru>
2022-03-18 14:00:48 +0700

    ClusteringUpd

--- Task 3 Clustering algorithm/Clustering algorithm.js
+++ Task 3 Clustering algorithm/Clustering algorithm.js
@@ -1,22 +1,24 @@
+// canvas settings:
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
 var widthConst = (window.innerWidth / 100);
 var heightConst = (window.innerHeight / 100);
-var buttonClear = document.getElementById('button2');
 canvas.width = widthConst * 70;
-canvas.height = heightConst * 70; // size of canvas
+canvas.height = heightConst * 70;
 
-
-function sizeWindow() {
+//func to show clusters count:
+function setClustersCount() {
     var counterClusters = document.getElementById("counterClusters").value;
     var windowOfCounter = document.getElementById("windowOfCounter");
     windowOfCounter.innerText = counterClusters;
 }
 
+// vars for array of coords:
 var coordsX = [];
 var coordsY = [];
 var counterPoints = 0;
 
+// to paint points:
 canvas.addEventListener('mousedown', function (e) {
     ctx.beginPath();
     var tempCoordX = e.clientX-280;
@@ -28,23 +30,23 @@ canvas.addEventListener('mousedown', function (e) {
     coordsY.push(tempCoordY);
 });
 
+//func to clustering:
 function alertFunc() {
     var counterClusters = document.getElementById("counterClusters").value;
     if (counterClusters > counterPoints) {
         alert("Sorry! Too many clusters!");
     }
     else {
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        ctx.fillStyle = "red";
-        for (let i = 0; i < coordsX.length; i++) {
-            ctx.beginPath();
-            ctx.arc(coordsX[i], coordsY[i], 10, 0, Math.PI * 2);
-            ctx.fill();
+        clustering();
         }
-    }
 }
 
+//func to clear the canvas and array:
 function clearing() {
+    var counterClusters = document.getElementById("counterClusters");
+    var windowOfCounter = document.getElementById("windowOfCounter");
+    windowOfCounter.innerText = "1";
+    counterClusters.value = 1;
     ctx.fillStyle = "black";
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     coordsY.length = 0;
@@ -52,3 +54,57 @@ function clearing() {
     counterPoints = 0;
 }
 
+function generateColor() {
+    return '#' + Math.floor(Math.random()*16777215).toString(16);
+}
+
+function clustering() {
+    var counterClusters = document.getElementById("counterClusters").value;
+    var centralPointsX = [];
+    var centralPointsY = [];
+    for(var i = 0; i < counterClusters; i++) {
+        centralPointsX[i] = Math.floor(Math.random()*canvas.width);
+        centralPointsY[i] = Math.floor(Math.random()*canvas.height);
+    }
+    var clusteringGroups = [];
+    for(var i = 0; i < counterClusters; i++) {
+        clusteringGroups[i] = [];
+    }
+    for(var i = 0; i < counterPoints; i++) {
+        var minLength = canvas.width+10;
+        var minClusterNum = 0;
+        for(var j = 0; j < counterClusters; j++) {
+            var tempLength = Math.sqrt(Math.pow((centralPointsX[j]-coordsX[i]),2) + Math.pow((centralPointsY[j]-coordsY[i]),2));
+            if (tempLength < minLength) {
+                minLength = tempLength;
+                minClusterNum = j;
+            }
+        }
+        clusteringGroups[minClusterNum].push(i);
+    }
+    var flag = 0;
+    for(var i = 0; i < clusteringGroups.length; i++) {
+        if (clusteringGroups[i].length == 0) {
+            flag = 1;
+            break;
+        }
+    }
+    if (flag == 1) {
+        clustering();
+    }
+    else {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        for (var i = 0; i < counterClusters; i++) {
+            var colorNow = generateColor();
+            for (var j = 0; j < clusteringGroups[i].length; j++) {
+                ctx.fillStyle = colorNow;
+                ctx.beginPath();
+                ctx.arc(coordsX[clusteringGroups[i][j]], coordsY[clusteringGroups[i][j]], 10, 0, Math.PI * 2);
+                ctx.fill();
+            }
+            ctx.fillRect(centralPointsX[i], centralPointsY[i], 20, 20);
+        }
+        ctx.fillStyle = 'black';
+        console.log(clusteringGroups);
+    }
+}
